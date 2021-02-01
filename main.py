a2c6cce2f1a1a325af913d95a511fc54a1f15792
Jackson <59755586+RandomNrrd@users.noreply.github.com>
2021-02-01 10:55:13 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+a7 = ["1", "9", "d", "a", "3", "D", "A"]
+b9 = ["2", "6", "4", "h", "e", "H", "E"]
+k11 = ["0", "5", "7", "k", "l", "K", "L"]
+o3 = ["8", "b", "c", "f", "w", "g", "B", "C", "F", "W","G" ]
+u5 = ["i", "j", "m", "v", "I", "J", "M", "V", "I"]
+c4 = ["n", "t", "q", "z", "N", "T", "Q", "Z"]
+p2 = ["r", "p", "o", "u", "x", "y", "R", "P", "O", "U","X", "Y"]
+d6 = [ "!", "@", "#", "$", "%", "^", "&", "*",]
+print("Break the cipher...")
+print("b9, b9, k11, k11, p2, d6")
+print("What is your awnser: ")
+awnser = input()
+if awnser == "Hello!":
+  print("Correct!")
+else:
+    print("Wrong try again")
